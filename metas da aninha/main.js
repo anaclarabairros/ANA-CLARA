3c81ab82de0f28df74f90eb5c6622da881d12648
Ana clara <128058331+anaclarabairros@users.noreply.github.com>
2024-04-29 11:49:14 -0300

    Add files via upload

--- metas da aninha/main.js
+++ metas da aninha/main.js
@@ -1,4 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2026-03-05T00:00:00");
+const tempoAtual = new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -6,9 +10,13 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
         }
         //Remove a classe "ativo" de todos os botões.
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
         //Adiciona a classe ativo só no botão clicado.
     }
 }
+
+contadores[0].textContent=tempoObjetivo1-tempoAtual;
